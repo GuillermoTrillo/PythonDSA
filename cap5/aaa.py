cc1022442e1c4a08421614722670fbe17fef9734
Guillermo <guillermotoro61@gmail.com>
2025-01-15 21:58:40 -0300

    Comecando esto

---
+++ cap5/aaa.py
@@ -0,0 +1,8 @@
+def MainMenu(): 
+    print("~~~~~~~~ Escolha seu tipo de operação ~~~~~~~~\n")
+    print("+\n")
+    print("-\n")
+    print("x\n")
+    print("/\n")
+    print("~~~~~~~~ Fim-de-men ~~~~~~~~\n")
+    operation = input("Enter symbol: ")
