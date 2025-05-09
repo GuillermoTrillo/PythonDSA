294d66f55afa4c8e6f3ab94512c3b87982b9be47
Guillermo <guillermotoro61@gmail.com>
2025-05-09 20:24:38 -0300

    Starting tiny Python project

---
+++ ProjectOne/Main.py
@@ -0,0 +1,14 @@
+
+import ProjectOne.Text as Text
+
+class Main:
+    
+    text = Text.Text()
+    
+    def main():
+       Main.StartGame()
+
+    def StartGame():
+        Main.text.startMenu()
+        
+Main.main()
---
+++ ProjectOne/Text.py
@@ -0,0 +1,18 @@
+class Text():
+    #* Menus
+    
+    @staticmethod
+    def startMenu():
+        print("----------------------------")
+        print("1. Start Game")
+        print("0. Close Game")
+        print("----------------------------")
+        
+    @staticmethod
+    def difficulty():
+        print("----------------------------")
+        print("1. Easy (4 or 5 letter words)")
+        print("2. Medium (6 or 10 letter words)")
+        print("3. Hard (7+ letter words)")
+        print("0. Go back")
+        print("----------------------------")
