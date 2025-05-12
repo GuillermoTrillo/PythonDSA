3a460a66f29c9c3293550caf468c19087a52fb34
Guillermo <guillermotoro61@gmail.com>
2025-05-12 19:57:36 -0300

    gitignore added!

--- ProjectOne/Text.py
+++ ProjectOne/Text.py
@@ -1,15 +1,21 @@
+import os
+
 class Text:
     #* Menus
+    clear = lambda: os.system('cls')
     
     @staticmethod
     def startMenu():
+        Text.clear()
         print("----------------------------")
         print("1. Start Game")
         print("0. Close Game")
         print("----------------------------")
         
     @staticmethod
+    
     def difficulty():
+        Text.clear()
         print("----------------------------")
         print("1. Easy (4 or 5 letter words)")
         print("2. Medium (6 or 10 letter words)")
